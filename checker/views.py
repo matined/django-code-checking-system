c7374c202e55a26bed6c5fa3fb6c4145823109f6
Mateusz Nedzi <mateusz.nedzi@cloudfide.com>
2023-12-28 20:39:52 +0100

    templates added

--- checker/views.py
+++ checker/views.py
@@ -1,6 +1,6 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello World!")
+    return render(request=request, template_name="base.html")
